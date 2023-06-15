d39c711b491bda378e09a64def51d77209213823
shivam-appboxer <shivam@appboxer.com.au>
2023-06-15 22:34:41 +0530

    fixed bugs

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 //   useGetProductsMutation,
 //   useGetProductsQuery,
 // } from "@/redux/features/Product/productSlice";
-import ClientComponent from "@/components/ClientComponent";
+// import ClientComponent from "@/components/ClientComponent";
 import React from "react";
 // import { useEffect } from "react";
 
@@ -11,11 +11,11 @@ const getProducts1 = async () => {
   let data = await fetch("http://localhost:3000/api/products", {
     next: { revalidate: 0 },
   });
-  await new Promise((res) => {
-    setTimeout(() => {
-      res("hi");
-    }, 2000);
-  });
+  // await new Promise((res) => {
+  //   setTimeout(() => {
+  //     res("hi");
+  //   }, 2000);
+  // });
   data = await data?.json();
   return data;
   console.log(data);
