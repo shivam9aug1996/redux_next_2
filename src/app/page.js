811c3d2b15fef1e19110524dfc923075907fdbfb
shivam-appboxer <shivam@appboxer.com.au>
2023-06-15 23:01:50 +0530

    fixed error

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ import React from "react";
 // import { useEffect } from "react";
 
 const getProducts1 = async () => {
-  let data = await fetch("http://localhost:3000/api/products", {
+  let data = await fetch(`${process.env.API_URL}api/products`, {
     next: { revalidate: 0 },
   });
   // await new Promise((res) => {
@@ -28,14 +28,14 @@ const Home = async () => {
   //   getProducts();
   // }, []);
 
-  // let data = await getProducts1();
+  let data = await getProducts1();
 
   //console.log(isLoading, isSuccess, data);
   return (
     <>
       {/* <h3>{isLoading ? "Loading" : ""}</h3> */}
-      <h1>ghjk</h1>
-      {/* <ClientComponent data={data} /> */}
+      {/* <h1>ghjk</h1> */}
+      <ClientComponent data={data} />
     </>
   );
 };
