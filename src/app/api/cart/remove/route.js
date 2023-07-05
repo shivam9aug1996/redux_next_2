73d2edc9b730812cde026d96f3822322573ef2f1
shivam-appboxer <shivam@appboxer.com.au>
2023-07-05 07:35:41 +0530

    caching 1

--- src/app/api/cart/remove/route.js
+++ src/app/api/cart/remove/route.js
@@ -81,45 +81,4 @@ export async function POST(req, res) {
   }
 }
 
-export async function GET(req, res) {
-  await middleware(req);
-  const userIdParam = new URL(req.url).searchParams.get("userId");
-  if (!userIdParam) {
-    return NextResponse.json({ error: "User ID is required" }, { status: 400 });
-  }
-  try {
-    const authHeader = req.headers.get("authorization");
-    const token = authHeader && authHeader.split(" ")[1];
-    if (!token) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
 
-    // Verify the token and extract the user ID
-    const userId = await verifyToken(token);
-
-    // const items = await req.json();
-
-    // const orderItems = items?.map(({ name, price, quantity, productId }) => ({
-    //   name,
-    //   price: parseFloat(price || 0),
-    //   quantity: parseInt(quantity || 1),
-    //   productId,
-    // }));
-    // const order = {
-    //   userId: parseInt(userId),
-    //   date: new Date(),
-    //   items: orderItems,
-    // };
-
-    let database = await connectDB();
-    const orders = await database.collection("cart").find({ userId }).toArray();
-    console.log(orders);
-    return NextResponse.json({ orders }, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      { status: 500 }
-    );
-  }
-}
