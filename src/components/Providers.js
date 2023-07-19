16dd47c44d64aaf7c2fa9b97b3130a59ee536cb0
shivam-appboxer <shivam@appboxer.com.au>
2023-07-20 01:00:57 +0530

    fixed header and implement got to cart

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -1,18 +1,20 @@
 "use client";
 import { setAppStart } from "@/redux/features/Auth/authSlice";
 import store from "@/redux/store";
-import React, { useEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { Provider } from "react-redux";
 import RouteMiddleware from "../app/RouteMiddleware";
 import Footer from "./Footer";
- import Header from "./Header";
-import dynamic from 'next/dynamic'
+import Header from "./Header";
+import dynamic from "next/dynamic";
 import { setAppStartCart } from "@/redux/features/Cart/cartSlice";
-//const Header = dynamic(() => import('./Header'))
+// const Header = dynamic(() => import('./Header'))
 
 const Providers = ({ children }) => {
-  // store.dispatch(setAppStart());
-  // store.dispatch(setAppStartCart())
+  useLayoutEffect(() => {
+    store.dispatch(setAppStart());
+    store.dispatch(setAppStartCart());
+  }, []);
   // useEffect(() => {
   //   store.dispatch(setAppStart());
   // }, []);
