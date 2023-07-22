45d6cb753da868fa3608ed6685e4d4122e98701d
shivam-appboxer <shivam@appboxer.com.au>
2023-07-22 14:12:39 +0530

    fix

--- src/app/product/[productId]/page.js
+++ src/app/product/[productId]/page.js
@@ -3,8 +3,6 @@ import React from "react";
 import Button from "./Button";
 import ImageClient from "./ImageClient";
 
-//export const dynamicParams = true
-
 const page = async ({ params }) => {
   console.log("hi");
   let url = "";
@@ -13,39 +11,17 @@ const page = async ({ params }) => {
   } else {
     url = `${process.env.API_URL}api/product`;
   }
-  // url = `${process.env.API_URL_PREVIEW}api/product`;
   let res = await fetch(`${url}?productId=${params.productId}`);
 
-  //console.log(process.env.NODE_ENV,process.env.API_URL,url)
   res = await res?.json();
-  // await new Promise((res)=>setTimeout(() => {
-  //   res("hi")
-  // }, 5000))
   const { product } = res;
   console.log("mjhgtr456789o", product);
   return (
     <div className="w-full p-4">
       <div className="border border-gray-300 rounded p-4 flex flex-col items-start">
-        {/* <img
-          src={product.image}
-          alt={product.name}
-          className="h-auto max-w-full max-h-64 mb-2 object-contain"
-        /> */}
-        <ImageClient product={product}/>
-        {/* <Image
-                  onLoad={(e) => e.target.classList.add('loaded')}
-                    src={product?.image}
-                    alt={product?.name}
-                    width={640}
-                    height={912}
-                    objectFit="cover"
-                    className="h-auto max-w-full max-h-64 mb-2 object-contain"
-                    loading="lazy"
-                    className="fade-in"
-                  /> */}
+        <ImageClient product={product} />
         <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
         <p className="text-gray-600">&#8377;{product?.price?.toFixed(2)}</p>
-
         <Button productId={params.productId} />
       </div>
     </div>
