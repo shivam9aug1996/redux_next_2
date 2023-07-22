a7c4cb715f5b2418446ccde36ecd8575f5326c62
shivam-appboxer <shivam@appboxer.com.au>
2023-07-22 19:23:20 +0530

    fix

--- src/app/product/[productId]/page.js
+++ src/app/product/[productId]/page.js
@@ -19,7 +19,7 @@ const page = async ({ params }) => {
     if (process.env.NODE_ENV == "development") {
       url = `http://localhost:3000/api/product`;
     } else {
-      url = `${process.env.API_URL}api/product`;
+      url = `api/product`;
     }
     let res = await fetch(`${url}?productId=${params.productId}`);
     res = await res?.json();
