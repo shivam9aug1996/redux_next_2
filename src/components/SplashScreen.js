1f7b7f88d27f8f7b2cddd5bcfd7a2dcbfc7b1953
shivam-appboxer <shivam@appboxer.com.au>
2023-07-26 23:47:10 +0530

    fixing recaptcha key

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,6 +1,9 @@
 // components/SplashScreen.js
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import styles from "./SplashScreen.module.css";
+ import cartPic from '../../public/cart.png'
+
 
 const SplashScreen = () => {
   const [show, setShow] = useState(true);
@@ -17,6 +20,7 @@ const SplashScreen = () => {
   return show ?  <div className={styles.splash}>
   <h1 className={styles.title}>Welcome to My Website</h1>
   <p className={styles.subtitle}>FastBuy</p>
+  <Image src={cartPic} width={200} height={200} style={{opacity:0.8}} priority />
 </div> : null;
 };
 
