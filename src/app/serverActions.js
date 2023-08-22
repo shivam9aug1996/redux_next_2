3b269ebc1d72414d44e678a4d80825007f75e653
shivam-appboxer <shivam@appboxer.com.au>
2023-08-22 23:28:40 +0530

    converted client to server component

--- src/app/serverActions.js
+++ src/app/serverActions.js
@@ -4,7 +4,7 @@ export const getProducts = async (page=1) => {
   if (process.env.NODE_ENV == "development") {
     url = `http://localhost:3000/api/products?page=${page}`;
   } else {
-    url = `${process.env.API_URL}api/products`;
+    url = `${process.env.API_URL}api/products?page=${page}`;
   }
   let data = await fetch(url,{cache:"force-cache"});
   data = await data?.json();
