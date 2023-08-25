11b157f7feaefbbef1bd9f2eee4ae20d01154b99
shivam-appboxer <shivam@appboxer.com.au>
2023-08-25 18:08:16 +0530

    implemented address

--- src/components/LoaderFull.js
+++ src/components/LoaderFull.js
@@ -9,7 +9,6 @@ const LoaderFull = () => {
       {/* <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div> */}
       {/* <h2 className="text-center text-xl text-gray-900">Loading...</h2> */}
       <div className={styles.spinner}></div>
-   
     </div>
     </>
   )
