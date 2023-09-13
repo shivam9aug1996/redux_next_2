ae5fdb061f238fabe754668db25eec688325c911
shivam-appboxer <shivam@appboxer.com.au>
2023-09-13 16:11:09 +0530

    integrated search functionailty in product  api

--- src/app/api/suggestions/route.js
+++ src/app/api/suggestions/route.js
@@ -9,9 +9,9 @@ import { NextResponse } from "next/server";
 export async function GET(req, res) {
   await middleware(req);
   const searchKeyword = new URL(req.url).searchParams.get("search_keyword");
-  if (!searchKeyword) {
-    return NextResponse.json({ error: "Search keyword is required" }, { status: 400 });
-  }
+  // if (!searchKeyword) {
+  //   return NextResponse.json({ error: "Search keyword is required" }, { status: 400 });
+  // }
   try {
     // const authHeader = req.headers.get("authorization");
     // const token = authHeader && authHeader.split(" ")[1];
@@ -27,21 +27,21 @@ export async function GET(req, res) {
 
     // Remove extra spaces between words and trim spaces from the beginning and end
     const cleanedSearchKeyword = searchKeyword
-      .replace(/\s+/g, ' ') // Replace multiple spaces with a single space
-      .trim();
+      ?.replace(/\s+/g, ' ') // Replace multiple spaces with a single space
+      ?.trim();
 
     // Split the cleaned searchKeyword into individual keywords
-    const keywords = cleanedSearchKeyword.split(' ');
+    const keywords = cleanedSearchKeyword?.split(' ');
 
     // Create an array of regex patterns to match each keyword
-    const regexPatterns = keywords.map((keyword) => new RegExp(keyword, 'i'));
+    const regexPatterns = keywords?.map((keyword) => new RegExp(keyword, 'i'));
 
     // Create an aggregation pipeline to retrieve suggestions
     const aggregationPipeline = [
       {
         $match: {
           name: {
-            $regex: regexPatterns[0], // Use the first regex pattern
+            $regex: regexPatterns?.[0], // Use the first regex pattern
           },
         },
       },
@@ -59,7 +59,7 @@ export async function GET(req, res) {
     let suggestions = await productCollection.aggregate(aggregationPipeline).toArray();
 
     // If there are suggestions, add a suggestion for the entire cleaned searchKeyword
-    if (suggestions?.length > 0&&cleanedSearchKeyword!==suggestions?.[0]?.name) {
+    if (suggestions?.length > 0&&cleanedSearchKeyword!==suggestions?.[0]?.name&&cleanedSearchKeyword!=="") {
       suggestions = [{ name: cleanedSearchKeyword }, ...suggestions];
     }
 
