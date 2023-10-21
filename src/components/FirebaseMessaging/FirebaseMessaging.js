0097445bfbdef3b6762338b97ecac18184090396
shivam-appboxer <shivam@appboxer.com.au>
2023-10-21 15:14:21 +0530

    push notification trying

--- src/components/FirebaseMessaging/FirebaseMessaging.js
+++ src/components/FirebaseMessaging/FirebaseMessaging.js
@@ -22,9 +22,9 @@ const FirebaseMessaging = () => {
       
     }
   
-   setTimeout(() => {
-    loadData();
-   }, 2000);
+  //  setTimeout(() => {
+  //   loadData();
+  //  }, 2000);
   },[])
   // useEffect(() => {
   //   let messaging = getMessaging(getApp());
